b52d1e4929e336064b8db035b8ae1f3a9d9a1c94
Rakshit <rakshitg600@gmail.com>
2025-02-02 03:25:34 +0530

    shifted logo file location

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,7 +5,7 @@ import LinearGradient from 'react-native-linear-gradient';
 const Menu = () => (
     <LinearGradient colors={['#fce4ec', '#f8bbd0']} style={styles.container}>
         <View style={styles.content}>
-            <Image source={require('../images/logo.png')} style={styles.image}/>
+            <Image source={require('../assets/images/logo.png')} style={styles.image}/>
             <Text style={styles.title}>ForeignHer</Text>
             <Text style={styles.title}>अतिथि देवो भव</Text>
             <TouchableOpacity onPress={() => console.log("Chatbot")}>
